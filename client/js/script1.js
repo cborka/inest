150f4cc824f278e7e1bfcd5b6b53f3f8302607c3
Boris Ts. (cborka) <cborka777@yandex.ru>
2021-10-15 16:58:30 +0700

    2021-10-15 Тестил setInterval

--- client/js/script1.js
+++ client/js/script1.js
@@ -1,5 +1,6 @@
 function say(msg) {
-    alert(msg);
+    // alert(msg);
+    log(msg);
 }
 
 
@@ -42,7 +43,58 @@ function promise_test() {
         () => alert("Промис завершён")        // аргумент - функция без аргументов
     );
 
+}
 
+function printNumbers(from, to) {
 
+    log(`printNumbers(${from}, ${to})`);
+
+    let id = setInterval(() => {
+
+        log(from++);
+        if (from > to) {
+            clearInterval(id);
+        }
+    }, 1000);
+
+//    for(let i=from; i<=to; i++) {    }
 
 }
+
+function printNumbers2(from, to) {
+
+    setTimeout(function printOne() {
+
+        log('...' + from++);
+
+        if (from <= to) {
+            setTimeout(printOne, 1000);
+        }
+
+    }, 1000);
+}
+
+// primise по памяти, есть контакт
+function prom() {
+
+    let prom = new Promise(function prox(resolve, reject) {
+        reject(new Error('resolve not done'));
+        resolve('resolve done');
+    })
+    .then((msg) => {log(msg); return msg;})
+    .catch(log)
+}
+
+
+// Лог
+function log(msg) {
+    document.getElementById('log').innerHTML += msg + '<br>';
+}
+
+// let z = 111;
+// log('z='+z);
+// printNumbers(z, 117);
+// //printNumbers2(511, 517);
+// setTimeout(() => log('z='+z), 10000);
+
+prom();
